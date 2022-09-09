3f97cafa86dbe1c317f49efb43363ffd00c9f7bc
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-09-09 21:35:54 +0530

    Create Note Service Project

---
+++ src/main/java/com/bridgelabz/fundoonoteservice/FundooNoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.fundoonoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundooNoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundooNoteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fundoonoteservice/FundooNoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fundoonoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundooNoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
